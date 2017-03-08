6d38d5c78191e8d3459b9f79447c753a96475a56
fatman622 <olegbabiy.ob@gmail.com>
2017-03-08 11:59:08 +0200

    All is Work

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,38 @@
+module Api
+	module V1
+		class BooksController < ApplicationController
+			before_action :set_book, only: [:show, :destroy]
+			def index
+				render json: Book.all
+			end
+
+			def create
+				@book = Book.create(book_params)
+				if @book.save
+					render json: @book, status: 201
+				else
+					render json: { errors: @book.errors.full_messages }, status: 422
+			end
+
+			end
+
+			def show
+				render json: set_book
+			end
+
+			def destroy
+				@book.destroy
+				render :show, status: :ok
+			end
+
+			private
+			def set_book
+				@book = Book.find(params[:id])
+			end
+
+			def book_params
+				params.permit(:autor, :text)
+			end
+		end
+	end
+end
---
+++ app/serializers/book_serializer.rb
@@ -0,0 +1,3 @@
+class BookSerializer < ActiveModel::Serializer
+	attributes :id, :autor, :text
+end
