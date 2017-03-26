550d7418fd626ca26848f1f78ae2eec85ab87f79
fatman622 <olegbabiy.ob@gmail.com>
2017-03-27 00:18:47 +0300

    Scope with sql %

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ApplicationRecord
 	validates :text, :author, :pages, presence: true
 	validates :available, inclusion: { in: [ true, false ] }
 
-	scope :author, -> (author) { where author: author }
+	# scope :author, -> (author) { where author: author }
+	scope :author, lambda {|author| where("author like ?", "#{author}%")}
 	scope :available, -> (available) { where available: available }
 end
 
