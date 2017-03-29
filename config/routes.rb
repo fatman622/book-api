29bc27ad60f7bafb2e48a99f84ab217da3adbc7b
fatman622 <olegbabiy.ob@gmail.com>
2017-03-29 15:47:44 +0300

    Add elasticsearch to Book model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   apipie
+  get 'api/v1/search', to: 'api/v1/books#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	namespace :api do
 		namespace :v1 do
 			resources :books
-			get 'search', to: 'books#search'
 		end
 	end
 end
