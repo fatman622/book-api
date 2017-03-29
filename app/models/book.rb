a3fe9ec115d0ff2155d526c2f0562cb563fd81c3
fatman622 <olegbabiy.ob@gmail.com>
2017-03-29 16:21:16 +0300

    Add elasticsearch to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,6 +9,5 @@ class Book < ApplicationRecord
 	scope :author, lambda {|author| where("LOWER(author) like ?", "#{author}%")}
 	scope :available, -> (available) { where available: available }
 end
-Book.import
 # Book.import force: true
 
