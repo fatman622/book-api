2129df14e7b9806e03d783db50b3e547d15828b0
fatman622 <olegbabiy.ob@gmail.com>
2017-03-30 13:42:31 +0300

    elastic search with bonsai

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   apipie
-  # get 'api/v1/books/search', to: 'api/v1/books#search'
+  get 'api/v1/books/search', to: 'api/v1/books#search'
 	namespace :api do
 		namespace :v1 do
 			resources :books
