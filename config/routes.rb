74565f9d942988a78d491f56281702a86890b25b
fatman622 <olegbabiy.ob@gmail.com>
2017-04-08 14:01:23 +0300

    add if authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   apipie
  
   get 'api/v1/books/search', to: 'api/v1/books#search'
-  mount_devise_token_auth_for 'User', at: 'api/v1/auth'
-
+ 
 	namespace :api do
 		namespace :v1 do
 			resources :books
+			mount_devise_token_auth_for 'user', at: 'auth'
 		end
 	end
 end
