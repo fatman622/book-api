2aef307b88b8f844b3166b170ec46cfeef34a396
fatman622 <olegbabiy.ob@gmail.com>
2017-04-08 11:57:46 +0300

    add if authenticate user

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -2,6 +2,7 @@ module Api
 	module V1
 		class BooksController < ApplicationController
 			include BooksDoc
+			include DeviseTokenAuth::Concerns::SetUserByToken
 			before_action :authenticate_user!
 			before_action :set_book, only: [:show, :destroy]
 			
