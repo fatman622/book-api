f40341c62fb4f6b00739f07cd34ecc0929a9e501
Oleg Babiy <olegbabiy.ob@gmail.com>
2017-04-19 14:42:14 +0300

    Add creating profile with User

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
-	belongs_to :user, optional: true
+	belongs_to :user
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
